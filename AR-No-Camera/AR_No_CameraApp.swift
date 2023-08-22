aa4ac47ff2851b875edcfbfe78a261e2458a9abe
Taylor <tsmith1024@gmail.com>
2023-08-22 14:43:36 -0500

    Initial Commit

---
+++ AR-No-Camera/AR_No_CameraApp.swift
@@ -0,0 +1,17 @@
+//
+//  AR_No_CameraApp.swift
+//  AR-No-Camera
+//
+//  Created by Taylor Smith on 8/22/23.
+//
+
+import SwiftUI
+
+@main
+struct AR_No_CameraApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
